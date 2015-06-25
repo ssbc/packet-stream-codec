e0d8762b8a32f52ed15ad96ad984683298e9e99a
Dominic Tarr <dominic.tarr@gmail.com>
2015-06-25 16:24:25 +0200

    test error in a request callback

--- test/index.js
+++ test/index.js
@@ -5,12 +5,21 @@ var split = require('pull-randomly-split')
 
 var psc = require('../')
 
+function flat (err) {
+  return {
+    message: err.message,
+    name: err.name,
+    stack: err.stack
+  }
+}
+
 var examples = [
   {req: 0, stream: false, end: false, value: ['event', {okay: true}]}, //an event
 
   {req: 1, stream: false, end: false, value: 'whatever'}, //a request
   {req:  2, stream: true, end: false, value: new Buffer('hello')}, //a stream packet
   {req: -2, stream: true, end: false, value: new Buffer('goodbye')}, //a stream response
+  {req: -3, stream: false, end: true, value: flat(new Error('intentional'))},
   {req:  2, stream: true, end: true, value: true}, //a stream packet
   {req: -2, stream: true, end: true, value: true} //a stream response
 ]
